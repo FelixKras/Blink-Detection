59dc297b797213f2906d3464e6a2a9c75a779912
FelixKras <felix.kras@gmail.com>
2018-11-09 08:52:51 +0200

    Refactoring (in the works)

--- BlinkDetect.py
+++ BlinkDetect.py
@@ -1,14 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 27 21:00:55 2018
 
-@author: Felix
-"""
-# USAGE
-# python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
-# python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
-
-# import the necessary packages
+# import the packages
 from scipy.spatial import distance as dist
 from imutils.video import FileVideoStream
 from imutils.video import VideoStream
@@ -21,39 +12,77 @@ import dlib
 import cv2
 
 
-def eye_aspect_ratio(eye):
-	# compute the euclidean distances between the two sets of
-	# vertical eye landmarks (x, y)-coordinates
-	A = dist.euclidean(eye[1], eye[5])
-	B = dist.euclidean(eye[2], eye[4])
-
-	# compute the euclidean distance between the horizontal
-	# eye landmark (x, y)-coordinates
-	C = dist.euclidean(eye[0], eye[3])
-
-	# compute the eye aspect ratio
-	ear = (A + B) / (2.0 * C)
-
-	# return the eye aspect ratio
-	return ear
 
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=False,
-				help="path to facial landmark predictor")
-ap.add_argument("-v", "--video", type=str, default="",
-				help="path to input video file")
-args = vars(ap.parse_args())
+class ConstsClass:
+	# eye aspect ratio for a blink
+	# number of consecutive frames the eye must be below the threshold
+	EYE_AR_THRESH = 0.3
+	EYE_AR_CONSEC_FRAMES = 3
+	FrameWidth=720
 
-# define two constants, one for the eye aspect ratio to indicate
-# blink and then a second constant for the number of consecutive
-# frames the eye must be below the threshold
-EYE_AR_THRESH = 0.3
-EYE_AR_CONSEC_FRAMES = 3
+	def CalcEAR(self,eye):
+		# compute the euclidean distances between the two sets of
+		# vertical eye landmarks (x, y)-coordinates
+		A = dist.euclidean(eye[1], eye[5])
+		B = dist.euclidean(eye[2], eye[4])
+
+		# compute the euclidean distance between the horizontal
+		# eye landmark (x, y)-coordinates
+		C = dist.euclidean(eye[0], eye[3])
+
+		# compute the eye aspect ratio
+		ear = (A + B) / (2.0 * C)
+
+		# return the eye aspect ratio
+		return ear
+
+class ImageProcUtil:
+
+	def ImproveLowLight_Average(self, frameIn):
+
+		NumFramesToAverage = 5
+		average_frame = np.zeros(frameIn.shape,dtype=float)
+		average_frame += frameIn
+		for i in range(1, NumFramesToAverage - 1):
+			(grabbed, frame) = vs.read()
+			frame = imutils.resize(frame, width=frameIn.shape[1])
+			average_frame += frame
+		average_frame /= NumFramesToAverage
+		average_frame.astype(np.uint8)
+		average_normalized_frame = cv2.normalize(average_frame, None, 0, 255, cv2.NORM_MINMAX)
+		frame = average_normalized_frame.astype(np.uint8)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		return gray
+
+	def ImproveLowLight_CLAHE(self, frame):
+
+		if len(frame.shape) == 3:
+			gray_before_CLAHE = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		elif len(frame.shape) == 2:
+			gray_before_CLAHE = frame
+
+		clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
+		gray_after_CLAHE = clahe.apply(gray_before_CLAHE)
+		return gray_after_CLAHE
+
+	def HandleEyeDetection(self,leftEye,rightEye):
+
+		leftEAR = ConstsClass.CalcEAR(leftEye)
+		rightEAR = ConstsClass.CalcEAR(rightEye)
+
+		# average the eye aspect ratio together for both eyes
+		ear = (leftEAR + rightEAR) / 2.0
+
+		# compute the convex hull for the left and right eye, then
+		# visualize each of the eyes
+		leftEyeHull = cv2.convexHull(leftEye)
+		rightEyeHull = cv2.convexHull(rightEye)
+
+		return leftEyeHull,rightEyeHull,ear
 
 # initialize the frame counters and the total number of blinks
-COUNTER = 0
+Local_Closed_Counter = 0
 TOTAL = 0
 
 # initialize dlib's face detector (HOG-based) and then create
@@ -80,44 +109,18 @@ vs = cv2.VideoCapture(0)
 
 time.sleep(0.5)
 
-
-def ImproveLowLight_Average(frameIn):
-
-	NumFramesToAverage = 10
-	average_frame = np.zeros(frameIn.shape)
-	average_frame+=frameIn
-
-	for i in range(1, NumFramesToAverage-1):
-		frame = vs.read()
-		frame = imutils.resize(frame, width=frameIn.shape[1])
-		average_frame += frame
-	average_frame /= NumFramesToAverage
-	#average_frame.astype(np.uint8)
-	average_normalized_frame = cv2.normalize(average_frame, None, 0, 255, cv2.NORM_MINMAX)
-	frame = average_normalized_frame.astype(np.uint8)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	return gray
-
-def ImproveLowLight_CLAHE(frame):
-
-	if len(frame.shape)==3:
-		gray_before_CLAHE=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	elif len(frame.shape)==2:
-		gray_before_CLAHE=frame
-
-	clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
-	gray_after_CLAHE = clahe.apply(gray_before_CLAHE)
-	return gray_after_CLAHE
-
 (grabed,frame)=vs.read()
 if frame is None:
 	print("[INFO] bad format frame, exiting")
 	quit()
 
-frame = imutils.resize(frame, width=720)
+frame = imutils.resize(frame, width=ConstsClass.FrameWidth)
+
 fourcc = cv2.VideoWriter_fourcc(*'DIVX')
 #fourcc = cv2.VideoWriter_fourcc(*'MJPG')
 out = cv2.VideoWriter('output.avi',fourcc, 20.0,(frame.shape[1],frame.shape[0]))
+
+UtilsClass= ImageProcUtil()
 
 # loop over frames from the video stream
 while True:
@@ -136,16 +139,17 @@ while True:
 	if frame is None:
 		print("[INFO] bad format frame, exiting")
 		break
-	frame = imutils.resize(frame, width=720)
 
-	improve_low_light=2
+	frame = imutils.resize(frame, width=ConstsClass.FrameWidth)
+
+	improve_low_light=1
 	if improve_low_light==1:
-		gray=ImproveLowLight_Average(frame)
+		gray=UtilsClass.ImproveLowLight_Average(frame)
 	elif improve_low_light==2:
-		gray=ImproveLowLight_CLAHE(frame)
+		gray=UtilsClass.ImproveLowLight_CLAHE(frame)
 	elif improve_low_light==3:
-		gray = ImproveLowLight_Average(frame)
-		gray = ImproveLowLight_CLAHE(gray)
+		gray = UtilsClass.ImproveLowLight_Average(frame)
+		gray = UtilsClass.ImproveLowLight_CLAHE(gray)
 	else:
 		gray= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -164,46 +168,38 @@ while True:
 		# coordinates to compute the eye aspect ratio for both eyes
 		leftEye = shape[lStart:lEnd]
 		rightEye = shape[rStart:rEnd]
-		leftEAR = eye_aspect_ratio(leftEye)
-		rightEAR = eye_aspect_ratio(rightEye)
 
-		# average the eye aspect ratio together for both eyes
-		ear = (leftEAR + rightEAR) / 2.0
-
-		# compute the convex hull for the left and right eye, then
-		# visualize each of the eyes
-		leftEyeHull = cv2.convexHull(leftEye)
-		rightEyeHull = cv2.convexHull(rightEye)
+		(leftEyeHull,rightEyeHull,ear)=UtilsClass.HandleEyeDetection(leftEye,rightEye)
 		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
 		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
 		# check to see if the eye aspect ratio is below the blink
 		# threshold, and if so, increment the blink frame counter
-		if ear < EYE_AR_THRESH:
-			COUNTER += 1
+		if ear < ConstsClass.EYE_AR_THRESH:
+			Local_Closed_Counter += 1
 
 		# otherwise, the eye aspect ratio is not below the blink
 		# threshold
 		else:
 			# if the eyes were closed for a sufficient number of
 			# then increment the total number of blinks
-			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+			if Local_Closed_Counter >= ConstsClass.EYE_AR_CONSEC_FRAMES:
 				TOTAL += 1
-
 			# reset the eye frame counter
 			COUNTER = 0
 
 		# draw the total number of blinks on the frame along with
 		# the computed eye aspect ratio for the frame
-		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
 	fps.stop()
 	cv2.putText(frame, "FPS: {:.2f}".format(1/fps.elapsed()), (10, 50),
 				cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
+
 	# show the frame
+
+	#bothImages=cv2.hconcat(frame,gray)
 	cv2.imshow("Frame", frame)
 
 	out.write(frame)
